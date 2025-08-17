de51ec82a8416c28e8a6235494553da0fe46917d
ethanwangzhikai <3052455224@qq.com>
2025-08-18 01:29:37 +0800

    feat: path

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  // publicPath: process.env.NODE_ENV === 'production'
-  //   ? '/ai/'  // 替换为你的仓库名称
-  //   : '/',
-  publicPath: '/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/ai/'  // 替换为你的仓库名称
+    : '/',
   outputDir: 'dist',
   assetsDir: 'assets',
   productionSourceMap: false,
